126ea80b11319475d2e86aa2eef67de02802cb72
Gabriel Rodrigues <33073778+gabrielroodriz@users.noreply.github.com>
2022-11-10 17:13:26 -0300

    feat: SignIn page
    Create SignIn Page
    Include metro.config
    Create Button Component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,24 @@
-import { Center, Text } from 'native-base';
+import { Center, Text, Icon } from 'native-base';
+import Fontisto from '@expo/vector-icons/Fontisto';
+
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
 
 export function SignIn() {
 	return (
-		<Center flex={1} bg='gray.900'>
-			<Text fontSize={24} color='white'>
-				SignIn ✌️😎
+		<Center flex={1} bg='gray.900' p={7}>
+			<Logo width={214} height={40} />
+			<Button
+				title='ENTRAR COM O GOOGLE'
+				type='SECUNDARY'
+				leftIcon={
+					<Icon as={Fontisto} name='google' color={'white'} size='md' />
+				}
+				mt={12}
+			/>
+			<Text color='white' textAlign='center' mt={4}>
+				Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação
+				de sua conta.
 			</Text>
 		</Center>
 	);
